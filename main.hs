d05b49d92149133113904079c26b464db349cbc1
oh-well <nlazo@est.frba.utn.edu.ar>
2019-04-24 19:43:22 -0300

    Planteo del inciso 2, no compila

--- main.hs
+++ main.hs
@@ -1,3 +1,4 @@
+--	INCISO 1	--
 data Auto = Auto {
 	nombre :: String,
 	nivelDeNafta :: Int,
@@ -5,3 +6,24 @@ data Auto = Auto {
 	nombreDeEnamorado :: String,
 	trucoParticular :: 
 } deriving Show
+
+--	 INCISO 2	--
+
+esVocal :: Char -> Bool
+esVocal letra = elem letra "aeiou"
+
+cantDeVocales :: String -> Int
+cantDeVocales = (length.(filter (esVocal)))
+
+velocidadDeTurbo :: Int -> Int
+velocidadDeTurbo nDeVocales
+	|	(nDeVocales>=1) && (nDeVocales<=2) = 15
+	|	(nDeVocales>=3) && (nDeVocales<=4) = 20
+	|	nDeVocales > 4 = 40
+
+incrementarVelocidad :: Auto -> Auto
+incrementarVelocidad auto = {	nombre,
+				nivelDeNafta,
+				velocidad + ((velocidadDeTurbo.cantDeVocales) nombreDeEnamorado),
+				trucoParticular
+}
