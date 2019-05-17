a1b8543072667fe535ccc274a2204e83ad835d53
Federico Di Chena <dichena.federico@gmail.com>
2019-05-16 22:05:20 -0300

    Tp Entrega 2
    Avances 3.0 - 3.1 - 3.2 - 3.3 (comentado)

--- cars/main.hs
+++ cars/main.hs
@@ -1,27 +1,135 @@
 import Text.Show.Functions -- Para mostrar <Function> en consola cada vez que devuelven una
 
 --	Punto 1	--
--- 1)
-type Velocidad = Float
+-- 1)import Text.Show.Functions -- Para mostrar <Function> en consola cada vez que devuelven una
+import Data.List -- Para métodos de colecciones que no vienen por defecto (ver guía de lenguajes)
+import Data.Maybe -- Por si llegan a usar un método de colección que devuelva “Just suElemento” o “Nothing”.
+
+--	INCISO 1	--
+
+type Velocidad = Int
 type Nafta = Float
 type Truco = Auto -> Auto
-type Criterio = Auto -> Float
+type Criterio = Auto -> Int
 type Metros = Float
+type Kilometros = Float
+type Trampa = Carrera -> Carrera
 
 data Auto = Auto {
-    nombre :: String,
-    nivelDeNafta :: Nafta,
-    velocidad :: Velocidad,
-    nombreDeEnamorado :: String,
-    trucoParticular :: Truco
+	nombre :: String,
+	nivelDeNafta :: Nafta,
+	velocidad :: Velocidad,
+	nombreDeEnamorado :: String,
+  trucoParticular :: Truco
+} deriving Show
+
+--3.1)
+
+data Carrera = Carrera {
+	vueltas :: Int,
+	longitudPista :: Kilometros,
+	participantes :: [Auto],
+	nombresPublico :: [String],
+  trampa :: Trampa
 } deriving Show
 
 --2)
+--deReversa :: Velocidad -> Truco
+--deReversa = aumentarNafta.(*0.2) en realidad solo cambiaria el tipo
+
+--potreroFunes = Carrera 3 5 [RochaMcQueen,biankerr,gushtav,rodra] ["Ronco", "Tinch", "Dodain"] sacarAlPistero
+
+--3.2)
+
+nuevosParticipantes :: (Carrera -> [Auto]) -> Carrera -> Carrera
+nuevosParticipantes criterio unaCarrera = cambiarParticipantes unaCarrera (criterio unaCarrera)
+
+--Para afectar a los participantes conviene pasarle la carrera y obtener los participantes o directamente pasarle los participantes??
+afectarParticipantes :: (Auto -> Auto) -> Carrera -> [Auto]
+afectarParticipantes criterio unaCarrera = map criterio (participantes unaCarrera)
+
+--seleccionarParticipantes :: (Auto -> Bool) ->  [Auto] -> [Auto]
+--seleccionarParticipantes criterio unaCarrera = filter criterio (participantes unaCarrera)
+
+sacarAlPistero :: Trampa
+sacarAlPistero = nuevosParticipantes sacarPrimerParticipante
+
+lluvia :: Trampa
+lluvia = nuevosParticipantes participantesDuranteLluvia
+
+neutralizarTrucos :: Trampa
+neutralizarTrucos  = nuevosParticipantes participantesNeutralizados
+
+pocaReserva :: Trampa
+pocaReserva  = nuevosParticipantes sacarParticipantesConPocaReserva
+
+podio :: Trampa
+podio  = nuevosParticipantes participantesPodio
+
+--Funciones Auxiliares para trampas
+
+participantesDuranteLluvia :: Carrera -> [Auto]
+participantesDuranteLluvia  = afectarParticipantes (disminuirVelocidad 10)
+
+participantesNeutralizados :: Carrera -> [Auto]
+participantesNeutralizados = afectarParticipantes (cambiaDeTruco inutilidad)
+
+sacarParticipantesConPocaReserva :: Carrera -> [Auto]
+sacarParticipantesConPocaReserva unaCarrera  = filter (not.tienePocaReserva) (participantes unaCarrera)
+
+participantesPodio :: Carrera -> [Auto]
+participantesPodio  = (take 3 . participantes)
+
+sacarPrimerParticipante :: Carrera -> [Auto]
+sacarPrimerParticipante  = (tail.participantes)
+
+tienePocaReserva :: Auto -> Bool
+tienePocaReserva  =  (naftaMenorAX 30)
+
+--3.3)
+
+sufrirTrampa :: Carrera -> Carrera
+sufrirTrampa unaCarrera = (trampa unaCarrera) unaCarrera
+
+
+realizanTruco :: Carrera -> Carrera
+realizanTruco = nuevosParticipantes participantesLuegoDeHacerTruco
+participantesLuegoDeHacerTruco ::  Carrera ->  [Auto]
+participantesLuegoDeHacerTruco  = afectarParticipantes realizarTruco
+realizarTruco :: Auto -> Auto
+realizarTruco unAuto = (trucoParticular unAuto) unAuto
+
+-- maniobraEnamorado = creo que habria que hacer guardas para aplicar por auto,
+-- porque si filtro la lista a los que puedan debolveria una lista diferente a la original
+
+--losVenSusEnamorades unaCarrera = seleccionarParticipantes (enamoradeEstaEnElPublico unaCarrera)
+--enamoradeEstaEnElPublico unaCarrera unAuto = any ((==).nombreDeEnamorado unAuto) (publico unaCarrera)
+--puedenRealizarUnTruco = seleccionarParticipantes puedeRealizarUnTruco
+
+--participantesLuegoDeUnaVuelta unaCarrera = afectarParticipantes (sufrirTrampa.maniobraEnamorado.combustibleLuegoDeVuelta unaCarrera)
+--combustibleLuegoDeVuelta unaCarrera =  (gastarNafta.calcularCombustibleVuelta unaCarrera)
+--calcularCombustibleVuelta unaCarrera unAuto = div (longitudPista unaPista) ((*10).velocidad)
+
+
+--darVuelta :: Carrera -> Carrera
+--darVuelta unaCarrera = nuevosParticipantes participantesLuegoDeUnaVuelta
+
+cambiarParticipantes :: Carrera ->  [Auto] -> Carrera
+cambiarParticipantes unaCarrera unosParticipantes = unaCarrera {participantes = unosParticipantes}
+
+inutilidad :: Auto -> Auto
+inutilidad = id
+
+cambiaDeTruco :: Truco -> Auto -> Auto
+cambiaDeTruco unTruco unAuto = unAuto {trucoParticular = unTruco}
+
 deReversa :: Metros -> Truco
 deReversa = aumentarNafta.(*0.2)
 
 aumentarNafta :: Nafta -> Auto -> Auto
 aumentarNafta cantidadNafta unAuto = unAuto {nivelDeNafta = nivelDeNafta unAuto + cantidadNafta}
+gastarNafta :: Nafta -> Auto -> Auto
+gastarNafta cantidadNafta unAuto = unAuto {nivelDeNafta = nivelDeNafta unAuto - cantidadNafta}
 
 impresionar :: Truco
 impresionar  = incrementarVelocidadSegun velocidad
@@ -34,6 +142,8 @@ incrementarVelocidadPorEnamorade = incrementarVelocidadSegun (velocidadDeTurbo.c
 
 aumentarVelocidad :: Velocidad -> Auto -> Auto
 aumentarVelocidad unaVelocidadAdicional unAuto = unAuto {velocidad = velocidad unAuto + unaVelocidadAdicional}
+disminuirVelocidad :: Velocidad -> Auto -> Auto
+disminuirVelocidad unaVelocidadAdicional unAuto = unAuto {velocidad = velocidad unAuto - unaVelocidadAdicional}
 
 nitro :: Truco
 nitro = aumentarVelocidad 15
@@ -68,6 +178,9 @@ velocidadDeTurbo nDeVocales
 -- Punto 3 --
 tieneNafta :: Auto -> Bool
 tieneNafta = (>0).nivelDeNafta
+
+naftaMenorAX :: Nafta ->  Auto -> Bool
+naftaMenorAX unaNafta = (<unaNafta).nivelDeNafta
 
 velocidadEsMenorACien :: Auto -> Bool
 velocidadEsMenorACien = (<100).velocidad
