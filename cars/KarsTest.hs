ce3d84e124fa7451aa63f46dcddc684d8fced8c9
delficostanzo <38230745+delficostanzo@users.noreply.github.com>
2019-05-19 18:48:54 -0300

    Update KarsTest.hs
    Agregue mas espacios asi queda mas prolijo

--- cars/KarsTest.hs
+++ cars/KarsTest.hs
@@ -33,7 +33,7 @@ darDosVueltas = darVuelta.darVuelta
 
 main :: IO ()
 main = hspec $ do
-	describe "Casos de prueba Kars" $ do
+	describe "Casos de prueba:" $ do
 		describe "Punto 3.0" $ do
 			it "La nafta del rochaMcQueen luego de hacer su truco favorito es 300" $ do
 				(nivelDeNafta.trucoParticular rochaMcQueen) rochaMcQueen `shouldBe` 300
@@ -45,41 +45,54 @@ main = hspec $ do
 			it "La cantidad de participantes en potreroFunes luego de sacarAlPistero es 3" $ do
 				(cantidadDeParticipantes) sacarAlPistero potreroFunes `shouldBe` 3
 
+
 			it "rochaMcQueen ya no participa en potreroFunes tras sacarAlPistero" $ do
 				 yaNoParticipaDeLaCarrera rochaMcQueen sacarAlPistero `shouldBe` True
+
 
 			it "La cantidad de participantes en potreroFunes luego de pocaReserva es 3" $ do
 				cantidadDeParticipantes pocaReserva potreroFunes `shouldBe` 3
 				
+				
 			it "El participante llamado “rodra” ya no debería estar entre los participantes de potreroFunes luego de aplicar pocaReserva" $ do
 				yaNoParticipaDeLaCarrera rodra pocaReserva `shouldBe` True
 				
+				
 			it "La cantidad de participantes luego de aplicar podio en potreroFunes es 3" $ do
 				 cantidadDeParticipantes podio potreroFunes `shouldBe` 3
-				 
+				
+				
 			it "La velocidad del último participante de potreroFunes (rodra) luego de la lluvia es 40" $ do
 				velocidadUtlimoParticipante lluvia potreroFunes `shouldBe` 40
+
 
 		describe "Punto 3.3" $ do
 			it "El nivel de nafta del primer participante (biankerr porque rochaMcQueen quedó afuera) luego de dar una vuelta en potreroFunes es 490" $ do
 				nivelDeNaftaPrimerParticipante darVuelta potreroFunes `shouldBe` 490
 
+
 			it "La velocidad del primer participante (biankerr porque rochaMcQueen quedó afuera) luego de dar una vuelta en potreroFunes es 40" $ do	
 				velocidadPrimerParticipante darVuelta potreroFunes `shouldBe` 40
 				
-			it "la cantidad de participantes tras dar dos vueltas en potrero funes es 2" $ do
+				
+			it "La cantidad de participantes tras dar dos vueltas en potrero funes es 2" $ do
 				cantidadDeParticipantes darDosVueltas potreroFunes `shouldBe` 2
+
 
 			it "Luego de dos vueltas, el nivelDeNafta del primer participante (gushtav) es 70" $ do
 				nivelDeNaftaPrimerParticipante darDosVueltas potreroFunes `shouldBe` 70
 
+
 			it "Rodra debe ser el único participante luego de correr la carrera de potreroFunes" $ do
+
 
 		describe "Punto 3.4" $ do
 			it "El ganador de potreroFunes es rodra" $ do
 
+
 		describe "Punto 3.5" $ do
 			it "La velocidad de rodra tras realizar elGranTruco con nitro, deReversa e impresionar es 140" $ do
+
 
 			it "El nivelDeNafta de rodra tras realizar elGranTruco con nitro, deReversa e impresionar es 13" $ do
 
