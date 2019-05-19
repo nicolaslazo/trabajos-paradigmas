b84f60c3bfd72ec7f7b119d579ddadc727fb95bb
delficostanzo <38230745+delficostanzo@users.noreply.github.com>
2019-05-19 17:51:00 -0300

    Update KarsTest.hs

--- cars/KarsTest.hs
+++ cars/KarsTest.hs
@@ -1,31 +1,42 @@
 import Kars
 import Test.Hspec
 
+cantidadDeParticipantes :: Trampa -> Carrera -> Int
+cantidadDeParticipantes trampa carrera = length.participantes.trampa carrera
+
+velocidadUtlimoParticipante :: Trampa -> Carrera -> Int
+velocidadUtlimoParticipante trampa carrera = velocidad.ultimoParticipante.trampa carrera
+
+ultimoParticipante :: Carrera -> Auto
+ultimoParticipante carrera =  last (participantes carrera)
+
 main :: IO ()
 main = hspec $ do
 	describe "Casos de prueba Kars" $ do
 		describe "Punto 3.0" $ do
 			it "La nafta del rochaMcQueen luego de hacer su truco favorito es 300" $ do
-				nivelDeNafta rochaMcQueen `shouldBe` 300
+				(nivelDeNafta.trucoParticular rochaMcQueen) rochaMcQueen `shouldBe` 300
 
 			it "La nafta de rodra tras deReversa es 10" $ do
-				deReversa rodra `shouldBe` 10
+				(nivelDeNafta.deReversa) rodra `shouldBe` 10
 
 		describe "Punto 3.2" $ do
 			it "La cantidad de participantes en potreroFunes luego de sacarAlPistero es 3" $ do
-				length (participantes potreroFunes) `shouldBe` 3
+				(cantidadDeParticipantes) sacarAlPistero potreroFunes `shouldBe` 3
 
 			it "rochaMcQueen ya no participa en potreroFunes tras sacarAlPistero" $ do
-				sacarAlPistero `shouldBe` True
+				 `shouldBe` True
 
 			it "La cantidad de participantes en potreroFunes luego de pocaReserva es 3" $ do
-
+				cantidadDeParticipantes pocaReserva potreroFunes `shouldBe` 3
+				
 			it "El participante llamado “rodra” ya no debería estar entre los participantes de potreroFunes luego de aplicar pocaReserva" $ do
 				
 			it "La cantidad de participantes luego de aplicar podio en potreroFunes es 3" $ do
+				 cantidadDeParticipantes podio potreroFunes `shouldBe` 3
 				 
 			it "La velocidad del último participante de potreroFunes (rodra) luego de la lluvia es 40" $ do
-				
+				velocidadUtlimoParticipante lluvia potreroFunes `shouldBe` 40
 
 		describe "Punto 3.3" $ do
 			it "El nivel de nafta del primer participante (biankerr porque rochaMcQueen quedó afuera) luego de dar una vuelta en potreroFunes es 490" $ do
